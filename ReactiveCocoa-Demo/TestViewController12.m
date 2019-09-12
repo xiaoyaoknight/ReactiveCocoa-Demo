7246693b304b4bea6f901f207176f9a769110c3b
dong.wang <dong.wang@yingzt.com>
2019-09-12 17:45:19 +0800

    点击事件

--- ReactiveCocoa-Demo/TestViewController12.m
+++ ReactiveCocoa-Demo/TestViewController12.m
@@ -19,16 +19,17 @@
     // Do any additional setup after loading the view.
     self.title = @"RAC-Event";
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(100, 100, 200, 50);
+    [button setTitle:@"我是使用rac按钮" forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor redColor];
+    [self.view addSubview:button];
+    [[button rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        NSLog(@"123");
+    }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
